e44e973dfa84589647a4c7c0eb32c4344b59ba0c
Abdi Berhe <abdiberhe@gmail.com>
2023-10-12 21:14:44 +0300

    [Stored Procedure]: Add new correction for a student

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,30 @@
+-- Create a stored procedure to add a bonus correction
+DROP PROCEDURE IF EXISTS AddBonus; 
+DELIMITER $$
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    DECLARE project_count INT DEFAULT 0;
+    DECLARE project_id INT DEFAULT 0;
+    
+    -- Check if the project already exists or create it
+    SELECT count(id)
+        INTO project_id
+        FROM projects
+        WHERE name = project_name;
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name)
+            VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    
+    SELECT id
+        INTO project_id
+        FROM projects
+        WHERE name = project_name;
+
+    -- Insert the new correction
+    INSERT INTO corrections (user_id, project_id, score)
+        VALUES (user_id, project_id, score);
+END;
+$$
+DELIMITER ;
