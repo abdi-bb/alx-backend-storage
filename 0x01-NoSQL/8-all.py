15e16823c87ddeaacc7c81741fefdeec6891d2be
Abdi Berhe <abdiberhe@gmail.com>
2023-10-17 23:41:27 +0300

    [Update]: Add module docs

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
-
-from pymongo import MongoClient
+'''
+Module: '8-all'
+'''
 
 
 def list_all(mongo_collection):
-    """
-    Lists all documents in a MongoDB collection.
-
-    Args:
-        mongo_collection (pymongo.collection.Collection): The collection to list documents from.
-
-    Returns:
-        list: A list of documents.
-    """
+    """Lists all documents in a MongoDB collection."""
     return [doc for doc in mongo_collection.find()]
