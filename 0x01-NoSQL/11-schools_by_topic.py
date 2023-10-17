252b644d3b88118a3d6864a37978817600b9d5d2
Abdi Berhe <abdiberhe@gmail.com>
2023-10-18 00:27:31 +0300

    [Update]; Use for loop

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,4 +6,4 @@ Module: '11-schools_by_topic'
 
 def schools_by_topic(mongo_collection, topic):
     '''Returns the list of docs having specific topic'''
-    return list(mongo_collection.find({'topic': topic}))
+    return [doc for doc in mongo_collection.find({'topic': topic})]
