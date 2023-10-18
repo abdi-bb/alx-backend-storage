424d70b10433239e409e6f910625f8af4a1221ad
Abdi Berhe <abdiberhe@gmail.com>
2023-10-18 23:10:33 +0300

    [Update]: Add decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,18 @@ Class named 'Cache'
 import redis
 from uuid import uuid4
 from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable):
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__  # Use the qualified name of the method as the key
+        count = int(self._redis.get(key) or 0)
+        count += 1
+        self._redis.set(key, count)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache():
@@ -17,6 +29,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Returns a string that is associated with a key'''
         key = str(uuid4())
