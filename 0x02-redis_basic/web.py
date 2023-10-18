bd2ce2b6942f2bc1dc5645ee4b39e2e6bcdb34d8
Abdi Berhe <abdiberhe@gmail.com>
2023-10-19 00:16:36 +0300

    [Function]: Implement cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -17,18 +17,18 @@ def data_cacher(method: Callable) -> Callable:
     '''Caches the output of fetched data.
     '''
     @wraps(method)
-    def invoker(url) -> str:
+    def wrapper(url) -> str:
         '''The wrapper function for caching the output.
         '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
+        redis_client.incr(f'count:{url}')
+        result = redis_client.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
+        redis_client.set(f'count:{url}', 0)
+        redis_client.setex(f'result:{url}', 10, result)
         return result
-    return invoker
+    return wrapper
 
 
 @data_cacher
