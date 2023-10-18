26ea8b1dcf78946f9897f7daa8a74453d4da6401
Abdi Berhe <abdiberhe@gmail.com>
2023-10-18 22:22:37 +0300

    [Update]: Use Union in store mehod

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,7 @@ Class named 'Cache'
 
 import redis
 from uuid import uuid4
+from typing import Union
 
 
 class Cache():
@@ -16,7 +17,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | int | float) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Returns a string that is associated with a key'''
         key = str(uuid4())
         self._redis.set(key, data)
